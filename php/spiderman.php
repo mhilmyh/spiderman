ebf40c51aca5306381db400d3f35381a6fae219e
mhilmyh <hilmy_21@apps.ipb.ac.id>
2020-05-25 23:18:43 +0700

    the spiderman decide to accept help from ironman which is simple_html_dom_parser

--- php/spiderman.php
+++ php/spiderman.php
@@ -1,6 +1,8 @@
 <?php
 
-class Spiderman
+use simplehtmldom_1_5\simple_html_dom;
+
+class Spiderman extends simple_html_dom
 {
     protected $curl = null;
     protected $url = '';
@@ -16,6 +18,22 @@ class Spiderman
         $this->url = $this->cleanUpURL($url);
         $this->printAll();
         $this->options = $options;
+    }
+
+    public function __destruct()
+    {
+        $this->clear();
+    }
+
+    public function clear()
+    {
+        $this->curl = null;
+        $this->url = '';
+        $this->scheme = '';
+        $this->host = '';
+        $this->endpoint = '';
+        $this->queries = [];
+        $this->options = [];
     }
 
     public function setURL($url = '')
@@ -144,5 +162,43 @@ class Spiderman
                 throw new ErrorException();
                 break;
         }
+    }
+
+    // helper functions
+    // -----------------------------------------------------------------------------
+    // get html dom from file
+    // $maxlen is defined in the code as PHP_STREAM_COPY_ALL which is defined as -1.
+    public function file_get_html($url, $use_include_path = false, $context = null, $offset = 0, $maxLen = -1, $lowercase = true, $forceTagsClosed = true, $target_charset = DEFAULT_TARGET_CHARSET, $stripRN = true, $defaultBRText = DEFAULT_BR_TEXT, $defaultSpanText = DEFAULT_SPAN_TEXT)
+    {
+        // We DO force the tags to be terminated.
+        $dom = new simple_html_dom(null, $lowercase, $forceTagsClosed, $target_charset, $stripRN, $defaultBRText, $defaultSpanText);
+        // For sourceforge users: uncomment the next line and comment the retreive_url_contents line 2 lines down if it is not already done.
+        $contents = file_get_contents($url, $use_include_path, $context, $offset);
+        // Paperg - use our own mechanism for getting the contents as we want to control the timeout.
+        //$contents = retrieve_url_contents($url);
+        if (empty($contents) || strlen($contents) > MAX_FILE_SIZE) {
+            return false;
+        }
+        // The second parameter can force the selectors to all be lowercase.
+        $dom->load($contents, $lowercase, $stripRN);
+        return $dom;
+    }
+
+    // get html dom from string
+    public function str_get_html($str, $lowercase = true, $forceTagsClosed = true, $target_charset = DEFAULT_TARGET_CHARSET, $stripRN = true, $defaultBRText = DEFAULT_BR_TEXT, $defaultSpanText = DEFAULT_SPAN_TEXT)
+    {
+        $dom = new simple_html_dom(null, $lowercase, $forceTagsClosed, $target_charset, $stripRN, $defaultBRText, $defaultSpanText);
+        if (empty($str) || strlen($str) > MAX_FILE_SIZE) {
+            $dom->clear();
+            return false;
+        }
+        $dom->load($str, $lowercase, $stripRN);
+        return $dom;
+    }
+
+    // dump html dom tree
+    public function dump_html_tree($node, $show_attr = true, $deep = 0)
+    {
+        $node->dump($node);
     }
 }
