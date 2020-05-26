951d9b3f06ed73d982d8420c813f1aac1fc3f0fd
mhilmyh <hilmy_21@apps.ipb.ac.id>
2020-05-26 13:37:57 +0700

    lets try another idea

--- php/runner.php
+++ php/runner.php
@@ -3,5 +3,4 @@
 include 'spiderman.php';
 
 $spidey = new Spiderman("https://stackoverflow.com/questions/28169042/php-curl-web-scraping");
-
-echo $spidey->file_get_html();
+echo $spidey->singleWebHit();
