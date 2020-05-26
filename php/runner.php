b434e6b80facd016c3339cb014a9c580928463eb
mhilmyh <hilmy_21@apps.ipb.ac.id>
2020-05-26 13:34:25 +0700

    hmm the weapon that ironman give for php/spiderman is not a good idea i think

--- php/runner.php
+++ php/runner.php
@@ -2,9 +2,6 @@
 
 include 'spiderman.php';
 
-$options = [
-    CURLOPT_HEADER => true,
-    CURLOPT_FOLLOWLOCATION => true
-];
-
 $spidey = new Spiderman("https://stackoverflow.com/questions/28169042/php-curl-web-scraping");
+
+echo $spidey->file_get_html();
--- php/spiderman.php
+++ php/spiderman.php
@@ -1,8 +1,8 @@
 <?php
 
-use simplehtmldom_1_5\simple_html_dom;
+use simple_html_dom;
 
-class Spiderman extends simple_html_dom
+class Spiderman
 {
     protected $curl = null;
     protected $url = '';
@@ -73,19 +73,6 @@ class Spiderman extends simple_html_dom
         return $response;
     }
 
-    public function setUpCURL()
-    {
-        $this->curl = curl_init();
-        curl_setopt($this->curl, CURLOPT_URL, $this->url);
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt_array($this->curl, $this->options);
-    }
-
-    public function closeCURL()
-    {
-        curl_close($this->curl);
-    }
-
     public function setQueryArray($queries = [])
     {
         $this->validate($queries, 'queries');
@@ -114,6 +101,19 @@ class Spiderman extends simple_html_dom
                 $this->ThrowError();
                 break;
         }
+    }
+
+    protected function setUpCURL()
+    {
+        $this->curl = curl_init();
+        curl_setopt($this->curl, CURLOPT_URL, $this->url);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt_array($this->curl, $this->options);
+    }
+
+    protected function closeCURL()
+    {
+        curl_close($this->curl);
     }
 
     protected function cleanUpURL($url)
@@ -162,43 +162,5 @@ class Spiderman extends simple_html_dom
                 throw new ErrorException();
                 break;
         }
-    }
-
-    // helper functions
-    // -----------------------------------------------------------------------------
-    // get html dom from file
-    // $maxlen is defined in the code as PHP_STREAM_COPY_ALL which is defined as -1.
-    public function file_get_html($url, $use_include_path = false, $context = null, $offset = 0, $maxLen = -1, $lowercase = true, $forceTagsClosed = true, $target_charset = DEFAULT_TARGET_CHARSET, $stripRN = true, $defaultBRText = DEFAULT_BR_TEXT, $defaultSpanText = DEFAULT_SPAN_TEXT)
-    {
-        // We DO force the tags to be terminated.
-        $dom = new simple_html_dom(null, $lowercase, $forceTagsClosed, $target_charset, $stripRN, $defaultBRText, $defaultSpanText);
-        // For sourceforge users: uncomment the next line and comment the retreive_url_contents line 2 lines down if it is not already done.
-        $contents = file_get_contents($url, $use_include_path, $context, $offset);
-        // Paperg - use our own mechanism for getting the contents as we want to control the timeout.
-        //$contents = retrieve_url_contents($url);
-        if (empty($contents) || strlen($contents) > MAX_FILE_SIZE) {
-            return false;
-        }
-        // The second parameter can force the selectors to all be lowercase.
-        $dom->load($contents, $lowercase, $stripRN);
-        return $dom;
-    }
-
-    // get html dom from string
-    public function str_get_html($str, $lowercase = true, $forceTagsClosed = true, $target_charset = DEFAULT_TARGET_CHARSET, $stripRN = true, $defaultBRText = DEFAULT_BR_TEXT, $defaultSpanText = DEFAULT_SPAN_TEXT)
-    {
-        $dom = new simple_html_dom(null, $lowercase, $forceTagsClosed, $target_charset, $stripRN, $defaultBRText, $defaultSpanText);
-        if (empty($str) || strlen($str) > MAX_FILE_SIZE) {
-            $dom->clear();
-            return false;
-        }
-        $dom->load($str, $lowercase, $stripRN);
-        return $dom;
-    }
-
-    // dump html dom tree
-    public function dump_html_tree($node, $show_attr = true, $deep = 0)
-    {
-        $node->dump($node);
     }
 }
